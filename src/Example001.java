66128c8081d507a8ef0adb763a348e3e5fdf0a95
Anastasiya Yagupova <nastyayagupova@mail.ru>
2023-04-18 21:04:24 +0300

    add changes

--- src/Example001.java
+++ src/Example001.java
@@ -1,9 +1,12 @@
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class Example001 {
     public static void main(String[] args){
     String s = new String("the sky is blue");
-    reverseWord(s);
+    //reverseWord(s);
+    //reverseWord2(s);
+    reverseWord3(s);
     }
     public static void reverseWord(String s){
         String[] array1 = s.split(" ");
@@ -14,5 +17,20 @@ public class Example001 {
             j++;
         }
         System.out.println(Arrays.toString(array2));
+    }
+    public static void reverseWord2(String s){
+        String[] array = s.split(" ");
+        for (int i = array.length-1; i >=0; i--) {
+            System.out.print(array[i] + ' ');
+        }
+    }
+    public static void reverseWord3(String s){
+        String[] array = s.split(" ");
+        Stream.Builder<String> str = Stream.builder();
+        for (int i = array.length-1; i >=0; i--) {
+        str.add(array[i]);
+        }
+        Stream<String> string = str.build();
+        string.forEach(System.out::println);
     }
 }
